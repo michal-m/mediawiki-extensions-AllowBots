93f6c6ee065630bde190716684d762ab84c28910
mmusial <mmusial@d0e49f55-ba13-a545-a6eb-e8ae84bb29ea>
2011-06-15 14:33:16 +0000

    first version of AllowBots extension
    git-svn-id: svn://localhost/Personal/mediawiki/branches/AllowBots@910 d0e49f55-ba13-a545-a6eb-e8ae84bb29ea

---
+++ AllowBots.php
@@ -0,0 +1,31 @@
+<?php
+if ( ! defined( 'MEDIAWIKI' ) )
+	die();
+
+/**#@+
+ * @author Michał Musiał <blitz@michalmusial.com>
+ * @copyright Copyright © 2011, Michał Musiał
+  */
+
+$wgExtensionCredits['parserhook'][] = array(
+        'path' => __FILE__,
+        'name' => 'Allow Bot Access',
+        'author' => 'Michał Musiał',
+        'version'=> '0.1'
+);
+
+if ( ! isset($wgBotsIPs) ) {
+	$wgBotsIPs = array();
+}
+
+if (in_array($_SERVER['REMOTE_ADDR'], $wgBotsIPs)) {
+	$wgGroupPermissions['*']['read'] = true;
+	# disable account creation
+	$wgGroupPermissions['*']['createaccount'] = false;
+	# disable anonymous editing/creation/talk
+	$wgGroupPermissions['*']['edit'] = false;
+	$wgGroupPermissions['*']['createpage'] = false;
+	$wgGroupPermissions['*']['createtalk'] = false;
+	# don't display IP header
+	$wgShowIPinHeader = false;
+}
